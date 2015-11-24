097899f2eb9126472185cac13872c814892f7453
Cole Robert Finnie <colefinnie@hotmail.com>
2015-11-23 23:20:23 -0500

    Finished the first page (feel free to make any changes)

--- _js/myscript.js
+++ _js/myscript.js
@@ -9,38 +9,22 @@ $(document).on("pagebeforeshow", "#listAll", function(){
 });
 
 function displayAll(xml) {
-	$("#listAllXML").append(
-		"<table>" +
-			"<tr>" +
-				"<th>ID</th>" +
-				"<th>Name</th>" +
-				"<th>City</th>" +
-				"<th>Available Docks</th>" +
-				"<th>Total Docks</th>" +
-				"<th>Latitude</th>" +
-				"<th>Longitude</th>" +
-				"<th>Status Value</th>" +
-				"<th>Status Key</th>" +
-				"<th>Available Bikes</th>" +
-			"</tr>"
-	);
 	$(xml).find("stationBeanList").each(function(){
 		$("#listAllXML").append(
-			"<tr>" +
-				"<td>" + $(this).find("id").text() + "</td>" +
-				"<td>" + $(this).find("stationName").text() + "</td>" +
-				"<td>" + $(this).find("stationName").attr("city") + "</td>" +
-				"<td>" + $(this).find("availableDocks").text() + "</td>" +
-				"<td>" + $(this).find("totalDocks").text() + "</td>" +
-				"<td>" + $(this).find("latitude").text() + "</td>" +
-				"<td>" + $(this).find("longitude").text() + "</td>" +
-				"<td>" + $(this).find("statusValue").text() + "</td>" +
-				"<td>" + $(this).find("statusKey").text() + "</td>" +
-				"<td>" + $(this).find("availableBikes").text() + "</td>" +
-			"</tr>"
+			"<div style='float:left; width:33%;'>" +
+			"<h1 class='center'>" + $(this).find("stationName").text() + "</h1>" +
+			"<table class='center' style='text-align:left;'>" +
+				"<tr><th>ID:</th><td>" + $(this).find("id").text() + "</td></tr>" +
+				"<tr><th>CITY: </th><td>" + $(this).find("stationName").attr("city") + "</td></tr>" +
+				"<tr><th>OPEN DOCKS: </th><td>" + $(this).find("availableDocks").text() + "</td></tr>" +
+				"<tr><th>TOTAL DOCKS: </th><td>" + $(this).find("totalDocks").text() + "</td></tr>" +
+				"<tr><th>LATITUDE: </th><td>" + $(this).find("latitude").text() + "</td></tr>" +
+				"<tr><th>LONGITUDE: </th><td>" + $(this).find("longitude").text() + "</td></tr>" +
+				"<tr><th>STATUS VALUE:&nbsp;&nbsp;</th><td>" + $(this).find("statusValue").text() + "</td></tr>" +
+				"<tr><th>STATUS KEY: </th><td>" + $(this).find("statusKey").text() + "</td></tr>" +
+				"<tr><th>OPEN BIKES: </th><td>" + $(this).find("availableBikes").text() + "</td></tr>" +
+			"</table><br>" +
+			"</div>"
 		);
 	});
-	$("#listAllXML").append(
-		"</table>"
-	);
 }
