d462ecbd43442a25d1f047adf1f11cdf4db80dd9
Cole Robert Finnie <colefinnie@hotmail.com>
2015-11-23 23:45:50 -0500

    Finished second page (THIS ONE JUST NEEDS IMAGES IN THE XML FILE)

--- _js/myscript.js
+++ _js/myscript.js
@@ -7,7 +7,6 @@ $(document).on("pagebeforeshow", "#listAll", function(){
 		success: displayAll
 	});
 });
-
 function displayAll(xml) {
 	$(xml).find("stationBeanList").each(function(){
 		$("#listAllXML").append(
@@ -27,4 +26,43 @@ function displayAll(xml) {
 			"</div>"
 		);
 	});
+}
+
+// DYNAMIC LIST
+$(document).on("pagebeforeshow", "#dynamicList", function(){
+	$.ajax({
+		type: "GET",
+		url: "projectXML06.xml",
+		dataType: "xml",
+		success: displayColl
+	});
+});
+function displayColl(xml){
+	$(xml).find("stationBeanList").each(function(){
+		$("#dynListXML").append(
+			"<section data-role='collapsible'>" +
+				"<h1 class='ui-btn'><span>" + $(this).find("stationName").text() + "</span></h1>" +
+				"<div class='ui-grid-a>" +
+					"<div class='ui-block-a'>" +
+						"<br /><table class='dynTable'>" +
+							"<tr><th>ID: </th><td>" + $(this).find("id").text() + "</td></tr>" +
+							"<tr><th>CITY: </th><td>" + $(this).find("stationName").attr("city") + "</td></tr>" +
+							"<tr><th>OPEN DOCKS: </th><td>" + $(this).find("availableDocks").text() + "</td></tr>" +
+							"<tr><th>TOTAL DOCKS: </th><td>" + $(this).find("totalDocks").text() + "</td></tr>" +
+							"<tr><th>LATITUDE: </th><td>" + $(this).find("latitude").text() + "</td></tr>" +
+							"<tr><th>LONGITUDE: </th><td>" + $(this).find("longitude").text() + "</td></tr>" +
+							"<tr><th>STATUS VALUE:&nbsp;&nbsp;</th><td>" + $(this).find("statusValue").text() + "</td></tr>" +
+							"<tr><th>STATUS KEY: </th><td>" + $(this).find("statusKey").text() + "</td></tr>" +
+							"<tr><th>OPEN BIKES: </th><td>" + $(this).find("availableBikes").text() + "</td></tr>" +
+						"</table>" +
+					"</div>" +
+					"<div class='ui-block-b>" +
+						"<p><img src='_images/" + $(this).find("stationImg").text() + "' width='200'></p>" +
+					"</div>" +
+				"</div>" +
+			"</section>"
+		);
+	});
+	
+	$("#dynListXML").collapsibleset("refresh");
 }
