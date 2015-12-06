82866ceadb33e524ca45ea22afbf59479458ac38
elvishknight1 <kyle.w.r@hotmail.com>
2015-12-06 02:02:41 -0800

    Small edit

--- _js/myscript.js
+++ _js/myscript.js
@@ -76,7 +76,7 @@ function displayColl(xml){
 	});
 });
 	function displayMap(xml){
-		$('#smap').append("<script type="text/javascript" src='http://maps.google.com/maps/api/js?v=3&sensor=false&language=en'");
+		$('#Smap').append("<script type="text/javascript" src='http://maps.google.com/maps/api/js?v=3&sensor=false&language=en'");
 	}
 
 	$("#dynListXML").collapsibleset("refresh");
