b69a80522540b81d116660b546a0b1d8945d90e6
TurquoiseGnome <john.w.petersons@gmail.com>
2015-12-06 15:36:30 -0500

    The map now works if you click on Station map and then refresh the page
    I'm working on making it appear without refreshing

--- _js/myscript.js
+++ _js/myscript.js
@@ -71,25 +71,9 @@ function displayColl(xml){
 
 	$("#dynListXML").collapsibleset("refresh");
 }
-var home = new google.maps.LatLng(32, 32);
-/*
-$(document).on("pagebeforeshow", "#stationMap", function(){
-	$.ajax({
-		type: "GET",
-		url: "projectXML06.xml",
-		dataType: "xml",
-		success: displayMap
-	});
-	// $('#sMap').gmap('refresh');
-});
-
-	function displayMap(xml){
-		$('#sMap').html("");
-		$('#sMap').append('<div id="resultMap" style="height:200px; width:200px; background-color:Lime;"></div>');
-		//$('#sMap').gmap({'center' : home, 'zoom':16});
-	}*/
+var home = new google.maps.LatLng(43, -79);
 	
-$(document).on("pageshow", "#stationMap", function(){
+$(document).on("pagebeforeshow", "#stationMap", function(){
 	$.ajax({
 		type: "GET",
 		url: "projectXML06.xml",
@@ -99,13 +83,32 @@ $(document).on("pageshow", "#stationMap", function(){
 });
 
 function displayMap(xml){
+	var x = 0;
+	var y = 0;
+	var xCount = 0;
+	var yCount = 0;
+	$(xml).find("stationBeanList").each(function(){
+		x += parseFloat($(this).find("latitude").text());
+		y += parseFloat($(this).find("longitude").text());
+		xCount += 1;
+		yCount += 1;
+	});
+	home = new google.maps.LatLng(x / xCount, y / yCount);
 	$('#sMap').html("");
 	$('#sMap').css({"height": "400px", "wdith": "200px", "background-color": "Lime"});
 	var mapOptions = {
 		center: home,
-		zoom: 16
+		zoom: 10,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var map = new google.maps.Map(document.getElementById("sMap"), mapOptions);
+	$(xml).find("stationBeanList").each(function(){
+		var loc = new google.maps.Marker ({
+			map : map,
+			animation : google.maps.Animation.DROP,
+			position : new google.maps.LatLng(parseFloat($(this).find("latitude").text()), parseFloat($(this).find("longitude").text()))
+		});
+	});
 }
 $(document).on("pagebeforeshow", "#totalDock", function(){
 	$.ajax({
